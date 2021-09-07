09644ed0beeb3756e2a37345bf4cd2fb17ca6607
khalidsy90 <khalidhamedi3@gmail.com>
2021-09-07 18:13:32 +0300

    link atlas

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,7 +1,7 @@
 // getting-started.js
 
 const mongoose = require('mongoose');
-mongoose.connect(`${process.env.MONGOLINK}/Chocolate`, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`${process.env.MONGOLINK}`, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
